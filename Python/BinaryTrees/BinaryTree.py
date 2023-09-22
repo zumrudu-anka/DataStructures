11b5f9e4bb42b97c1977f21436afe58bed667b1d
zumrudu-anka <osmandurdag@hotmail.com>
2023-09-22 16:58:31 +0300

    added binary tree

---
+++ Python/BinaryTrees/BinaryTree.py
@@ -0,0 +1,6 @@
+from Node import Node
+
+root = Node("A")
+root.left = Node("B")
+root.left.left = Node("D")
+root.right = Node("C")
--- Python/BinaryTrees/Node.py
+++ Python/BinaryTrees/Node.py
@@ -1,5 +1,11 @@
 class Node():
+    """
+        Binary Tree Nodes
+    """
     def __init__(self, data):
+        """
+            constructor
+        """
         self.value = data
         self.left = None
         self.right = None
